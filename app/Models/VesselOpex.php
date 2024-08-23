4ac7431ad92aad4af9ce1097d7965a9004013317
Rando Milla <rmilla@tecnoscientia.com>
2024-08-23 19:08:39 +0200

    add vesselOpex model

--- app/Models/VesselOpex.php
+++ app/Models/VesselOpex.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class VesselOpex extends Model
 {
     use HasFactory;
+
+    protected $table = 'vessel_opex';
+
+    protected $fillable = [
+        'vessel_id',
+        'date',
+        'expenses',
+    ];
+
+    public function vessel()
+    {
+        return $this->belongsTo(Vessel::class, 'vessel_id');
+    }
 }
